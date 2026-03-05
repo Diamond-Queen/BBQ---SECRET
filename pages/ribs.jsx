da9e4e44971fe7ea2aac50a94ce1627281f68d04
Diamond-Queen <q.neverhadthis@gmail.com>
2026-03-06 01:26:08 +0930

    ribs image fixed

--- pages/ribs.jsx
+++ pages/ribs.jsx
@@ -64,7 +64,7 @@ export default function Ribs() {
                 <img src="/images/ribs.jpeg" alt="Delicious Ribs"/>
               </div>
               <div className="gallery-item">
-                <img src="/images/ribs-sauced.jpeg" alt="Ribs with BBQ Sauce"/>
+                <img src="/images/ribs-sauced.jpg" alt="Ribs with BBQ Sauce"/>
               </div>
               <div className="gallery-item">
                 <img src="/images/ribs-nice.jpeg" alt="Fresh Ribs"/>
